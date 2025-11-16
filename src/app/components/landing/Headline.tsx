a776fac79a048b91dd728e0b468326d8ea095578
James Farr <jamesfarr1@sky.com>
2025-11-16 14:08:13 +0000

    Healline component created for code spliting

---
+++ src/app/components/landing/Headline.tsx
@@ -0,0 +1,10 @@
+
+export function Headline() {
+
+    return (
+        <div className="flex gap-2.5 flex-col">
+            <h1 className="text-[32px] leading-[38px] font-bold text-[#161823] tracking-normal">You could be owed up to £5,318.25* per car finance agreement.</h1>
+            <p className="font-normal text-[16px] leading-6 tracking-normal text-[#161823]">Check in under 60 seconds to see if you’re owed compensation. Use the free agreement finder to start your claim.</p>
+        </div>
+    )
+};
