953e3b00ca98a0d9c7ab6e223b572f0408b55244
James Farr <jamesfarr1@sky.com>
2025-11-16 14:10:13 +0000

    feat(Landing): New components Header & My agreements imported

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import Image from "next/image";
+import { useState } from "react";
 import { Checks } from "./components/landing/Checks";
 import { FAQs } from "./components/landing/FAQs";
-import Link from "next/link";
-import { useState } from "react";
+import { MyAgreements } from "./components/landing/MyAgreements";
+import { Headline } from "./components/landing/Headline";
+import { Trust } from "./components/landing/Trust";
+
+
 
 export default function Home() {
 
@@ -15,37 +18,11 @@ export default function Home() {
   return (
     <main className="my-2.5 flex flex-col">
       <div className="flex flex-col gap-2.5 border-b border-b-[#00000026] mx-4">
-        <div className="flex gap-2.5 flex-col">
-          <h1 className="text-[32px] leading-[38px] font-bold text-[#161823] tracking-normal">You could be owed up to £5,318.25* per car finance agreement.</h1>
-          <p className="font-normal text-[16px] leading-6 tracking-normal text-[#161823]">Check in under 60 seconds to see if you’re owed compensation. Use the free agreement finder to start your claim.</p>
-        </div>
+          <Headline />
         <div>
-          <Link href={"#"} className="pulse-button bg-[#FF004F] w-full flex h-[50px] items-center justify-center gap-3.5 text-white rounded-[7px] shadow-[0_0_0_4px_#FF004F4D]">
-            <Image src={"/images/magnifying-glass.png"} alt="Find My Agreements" width={17} height={17} className="w-[17px] h-[17px]" />
-            Find My Agreements
-          </Link>
+          <MyAgreements />
         </div>
-        <div>
-          <div className="flex my-3.25 flex-row items-center justify-center gap-6.5">
-            <Image
-              src={"/images/trustpilot.png"}
-              alt="Rated 4.8 out of 5 on Trustpilot"
-              width={93.56}
-              height={31.54}
-              className="w-[93.56px] h-[31.54px]"
-            />
-            <Image
-              src={"/images/secure-ssl.png"}
-              alt="Secure SSL Encryption"
-              width={75}
-              height={20.08}
-              className="w-[75px] h-[20.08px]"
-            />
-          </div>
-          <div className="my-3.25 w-[343px]">
-            <h2 className="text-2xl font-medium text-center">Average claim value: <span className="font-extrabold">£5,318.25*</span> per vehicle</h2>
-          </div>
-        </div>
+          <Trust /> 
       </div>
       <div className="px-6 py-2.5 mx-4">
         <Checks />
