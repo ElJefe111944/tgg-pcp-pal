e54e090fe667ad2dffc31f8fd5004dc98b1ad2b4
James Farr <jamesfarr1@sky.com>
2025-11-16 17:47:42 +0000

    Redux provider created in client folder and imported in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { TikTok_Sans } from "next/font/google"
 import "./globals.css";
-
+import { ReduxProvider } from "./store/providers/redux-provider";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
@@ -26,7 +26,9 @@ export default function RootLayout({
       <body className={tiktokSans.className}>
         <Header />
         <main className="w-full max-w-md">
-          {children}
+          <ReduxProvider>
+            {children}
+          </ReduxProvider>
         </main>
         <Footer />
       </body>
