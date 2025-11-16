25203803183d00b3cf750f53ebc1f45e5cc8ce7a
James Farr <jamesfarr1@sky.com>
2025-11-16 17:49:13 +0000

    address input error handling & dispatch to redux added

--- src/app/components/address/AddressForm.tsx
+++ src/app/components/address/AddressForm.tsx
@@ -1,20 +1,19 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
-import * as yup from "yup"
 import { yupResolver } from "@hookform/resolvers/yup";
+import { addressSchema, type AddressFormValues } from "../../validation/address";
+import { useAppDispatch, useAppSelector } from "@/app/store/hooks";
+import { setAddress } from "@/app/store/features/addressSlice";
 
-const addressSchema = yup.object({
-    addressLine1: yup.string().required("Address line 1 is required"),
-    addressLine2: yup.string().required("Address line 2 is required"),
-    town: yup.string().required("Town / City is required"),
-    county: yup.string().required("County is required"),
-  });
-
-  type AddressFormValues = yup.InferType<typeof addressSchema>;
 
 export function AddressForm(){
+
+    const router = useRouter();
+    const dispatch = useAppDispatch();
+    const addressState = useAppSelector((state) => state.address);
 
     const { 
         register,
@@ -23,23 +22,41 @@ export function AddressForm(){
      } = useForm<AddressFormValues>({
         resolver: yupResolver(addressSchema),
         mode: "onChange",
-
+        defaultValues: {
+            addressLine1: addressState.addressLine1,
+            addressLine2: addressState.addressLine2,
+            town: addressState.town,
+            county: addressState.county,
+          },
      })
 
-     const onSubmit = () => {
-
+     const onSubmit = (data: AddressFormValues) => {
+        dispatch(setAddress(data));
+        router.push("/personal-details");
      };
 
-     const inputStyles = () => "h-[50px] bg-[#F1F1F2] p-1.5 text-[16px] border-r-2";
+     const inputStyles = "h-[50px] bg-[#F1F1F2] p-1.5 text-[16px] border-r-2";
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} noValidate className="w-full flex flex-col gap-2.5">
-            <input className={`${inputStyles} h-[50px]`} type="text" id="addressLine1" {...register("addressLine1")} placeholder="Address Line 1" />
-            <input className={`${inputStyles} h-[50px]`} type="text" id="addressLine2" {...register("addressLine2")} placeholder="Address Line 2" />
-            <input className={`${inputStyles} h-[50px]`} type="text" id="town" {...register("town")} placeholder="Town/City" />
-            <input className={`${inputStyles} h-[50px]`} type="text" id="county" {...register("county")} placeholder="County" />
+            <input className={`${inputStyles}`} type="text" id="addressLine1" {...register("addressLine1")} placeholder="Address Line 1" />
+            {errors.addressLine1 && (
+                <p className="text-red-700">{errors.addressLine1.message}</p>
+            )}
+            <input className={`${inputStyles}`} type="text" id="addressLine2" {...register("addressLine2")} placeholder="Address Line 2" />
+            {errors.addressLine2 && (
+                <p className="text-red-700">{errors.addressLine2.message}</p>
+            )}
+            <input className={`${inputStyles}`} type="text" id="town" {...register("town")} placeholder="Town/City" />
+            {errors.town && (
+                <p className="text-red-700">{errors.town.message}</p>
+            )}
+            <input className={`${inputStyles}`} type="text" id="county" {...register("county")} placeholder="County" />
+            {errors.county && (
+                <p className="text-red-700" >{errors.county?.message}</p>
+            )}
             <p>Please check the details above are correct before continuing.</p>
-            <button className={`flex flex-row gap-2.5 justify-center items-center h-[50px] ${!isValid || isSubmitting ? "bg-[#B8B8BE]" : "bg-[#FF004F]"} text-white text-[15px] leading-6`} disabled={!isValid || isSubmitting}><span>Next</span> <Image src={"/images/address/arrow-right.png"} width={7} height={12.44} alt="Next" /></button>
+            <button type="submit" className={`flex flex-row gap-2.5 justify-center items-center h-[50px] ${!isValid || isSubmitting ? "bg-[#B8B8BE]" : "bg-[#FF004F]"} text-white text-[15px] leading-6`} disabled={!isValid || isSubmitting}><span>Next</span> <Image src={"/images/address/arrow-right.png"} width={7} height={12.44} alt="Next" /></button>
         </form>
     )
 }
