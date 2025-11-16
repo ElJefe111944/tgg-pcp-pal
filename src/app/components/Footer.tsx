5c50ef7290b4f45be7b8c18b4e055af046722494
James Farr <jamesfarr1@sky.com>
2025-11-16 14:07:18 +0000

    fix(footer): font size amend

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -47,9 +47,9 @@ export function Footer() {
             <span className="font-semibold">Privacy and Complaints</span>
             <br />
             By submitting a claim, you consent to Courmacs Legal Limited
-            processing your data in accordance with our <span className="underline">Privacy Policy</span>. For
+            processing your data in accordance with our <span className="underline text-[11px]">Privacy Policy</span>. For
             concerns, please review our{" "}
-            <span className="underline">Complaints Procedure</span>.
+            <span className="underline text-[11px]">Complaints Procedure</span>.
           </p>
 
           <p className={textClass}>
