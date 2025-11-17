4d6ca647475164056da56721f88baa1100493f2d
James Farr <jamesfarr1@sky.com>
2025-11-17 19:21:59 +0000

    Contact details slice added to redux store

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import addressReducer from "./features/addressSlice";
 import personalDetailsReducer from "./features/personalDetailsSlice";
+import contactDetailsReducer from "./features/contactDetailsSlice";
 
 export const store = configureStore({
     reducer: {
         address: addressReducer,
-        personalDetails: personalDetailsReducer
+        personalDetails: personalDetailsReducer,
+        contactDetails: contactDetailsReducer
 
     }
 });
