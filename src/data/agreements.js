1b79ca9756949500e7aa114ed7f0a952cb71a183
James Farr <jamesfarr1@sky.com>
2025-11-20 23:34:36 +0000

    data & images added for agreements

---
+++ src/data/agreements.js
@@ -0,0 +1,23 @@
+export const agreements = [
+    {
+        id: "1",
+        plate: "FF72 ZLA",
+        lender: "Santander Finance",
+        year: "2021",
+        carImageSrc: "/images/thank-you/santander-finance.png",
+    },
+    {
+        id: "2",
+        plate: "KY70 LMO",
+        lender: "Black Horse Finance",
+        year: "2020",
+        carImageSrc: "/images/thank-you/black-horse-finance.png",
+    },
+    {
+        id: "3",
+        plate: "SG65 YBA",
+        lender: "Audi Finance",
+        year: "2011",
+        carImageSrc: "/images/thank-you/audi-finance.png",
+    },
+];
