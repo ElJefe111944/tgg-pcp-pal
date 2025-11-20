756ecaaa9e43fd3c07fe695e29dbc84f34905dc1
James Farr <jamesfarr1@sky.com>
2025-11-20 23:35:09 +0000

    Legal component created & styled

---
+++ src/app/components/thank-you/Legal.tsx
@@ -0,0 +1,15 @@
+
+export function Legal() {
+
+    return (
+        <div className="flex flex-col gap-[15px]">
+            <h4 className="text-2xl leading-6 tracking-normal font-semibold text-[#161823]">Legal Obligations</h4>
+            <ul className="list-disc pl-5">
+                <li className="text-[12px] leading-[22px] tracking-normal text-[#161823CC] font-normal">
+                    By submitting your details, you have entered a legal claims process. It is important to review the confirmation email for more information about what to expect and the next steps in this process.
+                </li>
+                <li className="text-[12px] leading-[22px] tracking-normal text-[#161823CC] font-normal">Your Claim Value<br /> The average claim value is £5,318.25, with some clients receiving up to £10,446.46. However, please note that claim amounts depend on individual circumstances. The values mentioned are based on successful past claims.</li>
+            </ul>
+        </div>
+    )
+}
